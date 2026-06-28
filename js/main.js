e9dd5c76701b354ead7e4f71e2cfe9c8a687e525
Marina Rachello <mrachello@MacBookPro.communityfibre.co.uk>
2026-06-28 14:28:26 +0100

    Initial commit - portfolio site

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+import { initFitFooterHeadline } from "./fit-footer-headline.js";
 import { initSnapshots } from "./snapshots.js";
 import { initTitleAnimations } from "./titles.js";
 import { initFitHeroTitle } from "./fit-hero-title.js";
@@ -6,6 +7,7 @@ import { initCareer } from "./career.js";
 import { initSkills } from "./skills.js";
 
 initFitHeroTitle(document.querySelector(".intro__title"));
+initFitFooterHeadline(document.querySelector(".footer-headline"));
 initFooterReveal();
 initSnapshots(document.querySelector("[data-snapshots]"));
 initCareer();
